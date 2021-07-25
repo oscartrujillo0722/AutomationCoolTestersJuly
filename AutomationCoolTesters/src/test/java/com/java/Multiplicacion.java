23f08085a6c447bc3be94ef10cb931d16f1482d6
oskitrujillo <oscar.trujillo@globant.com>
2021-07-25 10:34:32 -0500

    Multiplicación añadida

---
+++ AutomationCoolTesters/src/test/java/com/java/Multiplicacion.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1= 3, n2=4, n3=8, multiplicacion = n1*n2*n3;
+		System.out.println(multiplicacion);
+		
+
+	}
+
+}
