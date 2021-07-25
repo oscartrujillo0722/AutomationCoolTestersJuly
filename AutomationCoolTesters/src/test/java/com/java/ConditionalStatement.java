8f0814a3f0e3bcabf4d5c13109ce440c35f742a4
oskitrujillo <oscar.trujillo@globant.com>
2021-07-25 10:23:35 -0500

    Test Push

---
+++ AutomationCoolTesters/src/test/java/com/java/ConditionalStatement.java
@@ -0,0 +1,20 @@
+package com.java;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int velocidad =61, limitevelocidad = 80; //km/h
+		if (velocidad>limitevelocidad) {
+			System.out.println("El coche va a exceso de velocidad, toma tu multa");
+		}
+		else { 
+				System.out.println("vas bien");
+			
+			}
+		}
+		
+
+	}
+
+
---
+++ AutomationCoolTesters/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,25 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int abc = 3;
+		char def = '\u00D4'; //Valor del codigo tomado de unicode u(codigo)
+		System.out.println(def);
+		System.out.println(abc);
+		String frase = "Holanadas";
+		System.out.println(def+frase);
+		
+		//operadores
+		
+		int x=3, y=5;
+		int z= x + y;
+		System.out.println(z);
+		
+		
+		
+
+	}
+
+}
